bc6b463ed5d627b997e52d83d98b0caa503f3e41
kihyyo <83176214+kihyyo@users.noreply.github.com>
2023-03-13 22:55:32 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,45 @@
+__menu = {
+    'uri': __package__,
+    'name': 'MAKE_YAML',
+    'list': [
+        {
+            'uri': 'main',
+            'name': 'Main',
+            'list': [
+                {"uri": "setting", "name": "설정"},
+                {"uri": "main", "name": "코드"},
+            ],
+        },
+        {
+        'uri': 'manual',
+        'name': '매뉴얼',
+        'list': [
+            {'uri':'README.md', 'name':'README.md'}
+        ]
+        },
+        {
+            'uri': 'log',
+            'name': '로그',
+        },
+    ]
+}
+
+
+setting = {
+    'filepath' : __file__,
+    'use_db': True,
+    'use_default_setting': True,
+    'home_module': 'main',
+    'menu': __menu,
+    'setting_menu': None,
+    'default_route': 'normal',
+}
+
+from plugin import *
+
+P = create_plugin_instance(setting)
+from .setup import P
+from .mod_main import ModuleMain
+P.set_module_list([ModuleMain])
+
+
