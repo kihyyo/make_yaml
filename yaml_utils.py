8f534d9c85e598c45087c04139ac96788421bb19
kihyyo <83176214+kihyyo@users.noreply.github.com>
2023-03-24 18:15:04 +0900

    Update yaml_utils.py

--- yaml_utils.py
+++ yaml_utils.py
@@ -114,7 +114,8 @@ class YAMLUTILS(object):
         show_data['extras'] = data['extra_info']
         for season in show_data['seasons']:
             season_number = season['index']
-            season_info = tmdbftv.info(tmdb_code+'_'+str(season_number))['data']
+            season_info = tmdbftv.info(tmdb_code+'_'+str(season_number))
+            logger.debug(season_info)
             for art in season_info['art']:
                 if art['aspect'] == 'poster':
                     season['posters'] = art['value']
